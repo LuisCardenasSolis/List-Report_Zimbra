3ec8ddba5219a26c84f717d05af888e7ffff8c1c
LuisCardenasSolis <luis.cardenas.s@tecsup.edu.pe>
2020-05-10 14:41:22 -0500

    Update reportLists.sh

--- reportLists.sh
+++ reportLists.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-PATH_REPORT="/tmp/report_list"
+PATH_REPORT="/tmp/report_list_$(date +%s)"
 
-if [ ! -d "$PATH_REPORT" ]; then
-    mkdir $PATH_REPORT
-fi
+mkdir $PATH_REPORT
 
 ZMPROV="/opt/zimbra/bin/zmprov"
 
-LISTS=$($ZMPROV gadl)
+if [ -z "$1" ]; then
+	LISTS=$($ZMPROV gadl)	
+elif [ $1 = "-l" ];then
+	shift
+	LISTS=$(echo "$1" | tr -d '[[:space:]]' | sed 's/,/ /g')
+else
+	echo "Parametros invalidos"
+	exit 1
+fi
 
-for list_name in $LISTS
+for list_name in $LISTS;
 do
-  echo  "============= LISTA : $list_name ============"
+echo  "============= LISTA : $list_name ============"
 
-  CONTADOR=0
+CONTADOR=0
 
-  ACCOUNTS=$($ZMPROV gdl $list_name members | grep -Ev '(^$|^#|members)')
-  FILE_PATH="${PATH_REPORT}/${list_name%@*}-${list_name#*@}.csv"
+ACCOUNTS=$($ZMPROV gdl $list_name members | grep -Ev '(^$|^#|members)')
+FILE_PATH="${PATH_REPORT}/${list_name}.csv"
 
-  echo "#,Cuenta,Nombre y Apellidos" | tee -a $FILE_PATH
-  for account in $ACCOUNTS
-  do
-    ((CONTADOR++))
+echo "#,Cuenta,Nombre y Apellidos,Tipo" | tee -a $FILE_PATH
+	for account in $ACCOUNTS; 
+	do
+	CONTADOR=$((CONTADOR+1))
+	
+	name_account=$( $ZMPROV ga $account displayName 2>/dev/null | grep -Ev '(^#|^$)'| sed 's/displayName: //')
+	if [ -z "${name_account}" ];then
+		# Comprobar si es una lista
+		$ZMPROV gdl $account &> /dev/null
+		if [ $? != 0 ];then
+		echo "${CONTADOR},${account},,CUENTA O LISTA NO EXISTENTE" | tee -a $FILE_PATH
+		else
+		echo "${CONTADOR},${account},,LISTA DE CORREO" | tee -a $FILE_PATH
+		fi
+	else
+	echo "${CONTADOR},${account},${name_account},CUENTA DE CORREO" | tee -a $FILE_PATH
+	fi
+	
+	done
 
-    name_account=$( $ZMPROV ga $account displayName | grep -Ev '(^#|^$)'| sed 's/displayName: //')
-
-    echo "${CONTADOR},${account},${name_account}" | tee -a $FILE_PATH
-  done
-
-  echo -e "\n"
+echo -e "\n"
 done
 
-echo "REPORTE GUARDADO EN : ${PATH_REPORT}/"
+echo "REPORTE GUARDADO : ${PATH_REPORT}"
